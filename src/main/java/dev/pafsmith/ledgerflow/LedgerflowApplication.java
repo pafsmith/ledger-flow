b34b4c0f2b6075a752b6d932d495879fe1ce7d49
Paul Smith <psmith@magna.co.uk>
2026-03-06 12:49:54 +0000

    initial spring boot commit

---
+++ src/main/java/dev/pafsmith/ledgerflow/LedgerflowApplication.java
@@ -0,0 +1,13 @@
+package dev.pafsmith.ledgerflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgerflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LedgerflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/pafsmith/ledgerflow/LedgerflowApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pafsmith.ledgerflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
