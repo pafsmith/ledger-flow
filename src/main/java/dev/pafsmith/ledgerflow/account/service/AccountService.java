b95db5395850c94ba0c9f110471dca31d989d58c
Paul Smith <paul@psmithdev.com>
2026-03-09 19:05:28 +0000

    feat: added service annotation to account service

--- src/main/java/dev/pafsmith/ledgerflow/account/service/AccountService.java
+++ src/main/java/dev/pafsmith/ledgerflow/account/service/AccountService.java
@@ -2,10 +2,12 @@ package dev.pafsmith.ledgerflow.account.service;
 
 import dev.pafsmith.ledgerflow.account.entity.Account;
 import dev.pafsmith.ledgerflow.account.repository.AccountRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
+@Service
 public class AccountService {
     private final AccountRepository accountRepository;
 
