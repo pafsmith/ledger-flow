27ffdfab09bd367fa1578c69ede234e85c481587
Paul Smith <psmith@magna.co.uk>
2026-03-11 10:56:41 +0000

    feat: Added custom handlers for exceptions

---
+++ src/main/java/dev/pafsmith/ledgerflow/common/exception/BadRequestException.java
@@ -0,0 +1,8 @@
+package dev.pafsmith.ledgerflow.common.exception;
+
+public class BadRequestException extends RuntimeException {
+
+  public BadRequestException(String message) {
+    super(message);
+  }
+}
---
+++ src/main/java/dev/pafsmith/ledgerflow/common/exception/ForbiddenException.java
@@ -0,0 +1,8 @@
+package dev.pafsmith.ledgerflow.common.exception;
+
+public class ForbiddenException extends RuntimeException {
+
+  public ForbiddenException(String message) {
+    super(message);
+  }
+}
