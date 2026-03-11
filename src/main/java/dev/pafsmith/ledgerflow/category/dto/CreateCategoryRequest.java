285933b616d863ad06a20de9dc69aa245c92d223
Paul Smith <psmith@magna.co.uk>
2026-03-11 15:17:58 +0000

    feat: category response and request dto

---
+++ src/main/java/dev/pafsmith/ledgerflow/category/dto/CreateCategoryRequest.java
@@ -0,0 +1,50 @@
+package dev.pafsmith.ledgerflow.category.dto;
+
+import java.util.UUID;
+
+import dev.pafsmith.ledgerflow.category.enums.CategoryType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class CreateCategoryRequest {
+
+  @NotNull(message = "User id is required")
+  private UUID userId;
+
+  @NotBlank(message = "Category name is required")
+  @Size(max = 100, message = "Category name must be 100 characters or fewer")
+  private String name;
+
+  @NotNull(message = "Category type is required")
+  private CategoryType type;
+
+  public CreateCategoryRequest() {
+
+  }
+
+  public UUID getUserId() {
+    return userId;
+  }
+
+  public void setUserId(UUID userId) {
+    this.userId = userId;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public CategoryType getType() {
+    return type;
+  }
+
+  public void setType(CategoryType type) {
+    this.type = type;
+  }
+
+}
