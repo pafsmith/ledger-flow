f1289c01e162d5c46ac003177629f4adf935f543
Paul Smith <psmith@magna.co.uk>
2026-03-11 23:03:01 +0000

    feat: ability to delete transactions by id

--- src/main/java/dev/pafsmith/ledgerflow/transaction/controller/TransactionController.java
+++ src/main/java/dev/pafsmith/ledgerflow/transaction/controller/TransactionController.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -70,5 +71,11 @@ public class TransactionController {
       @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
       @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
     return transactionService.getTransactionsForUserBetweenDates(userId, startDate, endDate);
+  }
+
+  @DeleteMapping("/{transactionId}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void deleteTransaction(@PathVariable UUID transactionId) {
+    transactionService.deleteTransaction(transactionId);
   }
 }
